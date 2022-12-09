32013ff2956267b95aa48645e18becb91df68f04
Arka Mondal <arkamondal1995@outlook.com>
2022-12-09 21:59:46 +0530

    Play With xPath Tag

--- src/learn_selenium/SelIntroduction.java
+++ src/learn_selenium/SelIntroduction.java
@@ -10,20 +10,15 @@ public class SelIntroduction {
 	public static void main(String[] args) throws InterruptedException {
 		
 		// chromedriver.exe -> Chrome browser
-		System.setProperty("webdriver.chrome.driver", "C://Users//Arka_007//Desktop//Driver//chromedriver.exe");
+		System.setProperty("web-driver.chrome.driver", "C://Users//Arka_007//Desktop//Driver//chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 		driver.manage().window().maximize();
 		Thread.sleep(5000);
-		driver.findElement(By.name("username")).sendKeys("Admin");
-		driver.findElement(By.name("password")).sendKeys("test123");
-		driver.findElement(By.xpath("//body/div[@id='app']/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/form[1]/div[3]/button[1]")).click();
-		Thread.sleep(2000);
-		String invalidPass = driver.findElement(By.xpath("//body/div[@id='app']/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/p[1]")).getText();
-		System.out.println(invalidPass);
-
-		// Open Hyperlink we used the LinkText.
-		driver.findElement(By.linkText("OrangeHRM, Inc")).click();
+		// Different Type of xPath
+		driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Admin");
+		driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
 
 		//driver.close();
 
